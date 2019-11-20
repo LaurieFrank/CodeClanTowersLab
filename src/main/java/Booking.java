30f9d29541e088ba072ca809ab8d0b0b5fd0d3aa
Laurie Frank <laurie360@hotmail.co.uk>
2019-11-20 15:23:20 +0000

    end of lab

---
+++ src/main/java/Booking.java
@@ -0,0 +1,15 @@
+import java.util.ArrayList;
+
+public class Booking {
+
+    private Booking booking
+    private Bedroom bedroom;
+    private int nights;
+
+
+    public Booking(Bedroom bedroom, int nights) {
+        this.bedroom = bedroom;
+        this.nights = nights;
+
+    }
+}
